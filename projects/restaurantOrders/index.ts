461c9fa320384abe1957ea2bcf9cd7c30ff265de
Glenn Law <Glennlaw-0@users.noreply.github.com>
2021-01-14 18:20:54 -0500

    added filtered orders function

--- projects/restaurantOrders/index.ts
+++ projects/restaurantOrders/index.ts
@@ -1,12 +1,49 @@
 import { restaurants, Restaurant } from "./restaurants";
 import { orders, Order, PriceBracket } from "./orders";
 
+
+
 /// Add your getMaxPrice() function below:
+const getMaxPrice = (price: PriceBracket): number => {
+  switch(price){
+    case PriceBracket.Low:
+      return 10.0;
+    case PriceBracket.Medium:
+      return 20.0;
+    case PriceBracket.High:
+      return 20.0;
+    default:
+      return 0
+  }
+}
+
 
 /// Add your getOrders() function below:
+const getOrders = (price: PriceBracket, orders: Order[][]) => {
+  let filteredOrders: Order[][] = [];
+
+
+    orders.forEach((restaurant: Order[]) => {
+    const result = restaurant.filter((order: Order) => order.price <= getMaxPrice(price))
+
+    filteredOrders.push(result)
+  })
+  return filteredOrders;
+}
+
+
 
 /// Add your printOrders() function below:
+const printOrders = (restaurants: Restaurant[], orders: Order[][]) => {
 
-/// Main
+
+//   restaurants.forEach((restaurant, index) => {
+//     if()
+//   })
+}
+
+
+
+// / Main
 // const elligibleOrders = getOrders(PriceBracket.Low, orders);
 // printOrders(restaurants, elligibleOrders);
